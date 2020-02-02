9772177050b79fed1edd79a1df20fe240af33572
kiseokbyun <qusrl0715@gmail.com>
2020-02-02 14:17:04 +0900

    programmers - 예산

---
+++ src/algorithmStudy/binarySearch/Budget.java
@@ -0,0 +1,44 @@
+package algorithmStudy.binarySearch;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+
+public class Budget {
+    public int solution(int[] budgets, int M) {
+        int[] jobs = new int[100000];
+        for (int i = 0; i < jobs.length; i++) {
+            jobs[i] = 0;
+        }
+
+        long sum = 0;
+        int maxBudget = 0;
+        for (int budget : budgets) {
+            if (maxBudget < budget) {
+                maxBudget = budget;
+            }
+            jobs[budget]++;
+            sum += budget;
+        }
+
+        while (sum > M) {
+            sum -= jobs[maxBudget];
+            jobs[maxBudget - 1] += jobs[maxBudget];
+            jobs[maxBudget] = 0;
+            maxBudget--;
+        }
+
+        return maxBudget;
+    }
+    
+    @Test
+    public void test() {
+        int[] budgets = {120, 110, 140, 150};
+        int M = 485;
+        int result = solution(budgets, M);
+        assertThat(result, equalTo(127));
+    }
+    
+}
